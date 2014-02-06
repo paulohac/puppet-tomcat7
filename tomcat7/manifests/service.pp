a38eab04fc398c98ba7a894898d2e3694abafe54
paulohac <paulohenrique.comp@gmail.com>
2014-02-06 13:55:48 -0200

    Update service.pp
    Add variables from params.pp

--- tomcat7/manifests/service.pp
+++ tomcat7/manifests/service.pp
@@ -1,14 +1,19 @@
 class tomcat7::service
  (
-  $service_name   = tomcat,
-  $service_enable = true,
-  $service_ensure = 'running',
-){
+  $service_name   = $tomcat7::params::service_name,
+  $service_enable = $tomcat7::params::service_enable,
+  $service_ensure = $tomcat7::params::service_ensure,
+  $service_status = $tomcat7::params::service_status,
+)inherits tomcat7::params {
 
-  service { 'tomcat':
+#hasstatus false to evaluate the service start command
+  service { '$service_name':
     ensure => $service_ensure,
     name   => $service_name,
     enable => $service_enable,
+    hasstatus => false,
+    status => $service_status,
+    hasrestart => true
   }
 }
 
