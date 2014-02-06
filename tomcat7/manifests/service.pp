0e4ba1e456e8c45bb8e822faea3c56c07577bef1
paulohac <paulohenrique.comp@gmail.com>
2014-02-06 17:19:39 -0200

    Update service.pp
    Add hasstatus false

--- tomcat7/manifests/service.pp
+++ tomcat7/manifests/service.pp
@@ -3,7 +3,6 @@ class tomcat7::service
   $service_name   = $tomcat7::params::service_name,
   $service_enable = $tomcat7::params::service_enable,
   $service_ensure = $tomcat7::params::service_ensure,
-  $service_status = $tomcat7::params::service_status,
 )inherits tomcat7::params {
 
 #hasstatus false to evaluate the service start command
@@ -12,7 +11,6 @@ class tomcat7::service
     name   => $service_name,
     enable => $service_enable,
     hasstatus => false,
-    status => $service_status,
     hasrestart => true
   }
 }
