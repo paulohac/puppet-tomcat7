116d5983d56f4af73c04383df49f6a3dc59e48fc
paulohac <paulohenrique.comp@gmail.com>
2014-02-06 18:58:44 -0200

    Update service.pp

--- tomcat7/manifests/service.pp
+++ tomcat7/manifests/service.pp
@@ -1,17 +1,21 @@
 class tomcat7::service
  (
-  $service_name   = $tomcat7::params::service_name,
-  $service_enable = $tomcat7::params::service_enable,
-  $service_ensure = $tomcat7::params::service_ensure,
+  $service_name    = $tomcat7::params::service_name,
+  $service_enable  = $tomcat7::params::service_enable,
+  $service_ensure  = $tomcat7::params::service_ensure,
+  $service_status  = $tomcat7::params::service_status,
+  $service_restart = $tomcat7::params::service_restart,
 )inherits tomcat7::params {
 
 #hasstatus false to evaluate the service start command
   service { '$service_name':
-    ensure => $service_ensure,
-    name   => $service_name,
-    enable => $service_enable,
-    hasstatus => false,
-    hasrestart => true
+    ensure     => $service_ensure,
+    name       => $service_name,
+    enable     => $service_enable,
+    hasstatus  => $service_status,
+    hasrestart => $service_restart,
+    require    => Class['tomcat7::config']
   }
 }
+
 
