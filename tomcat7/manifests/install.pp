78a06ec247e5331e510041c08209f7461b46289d
paulohac <paulohenrique.comp@gmail.com>
2014-02-08 22:51:58 -0200

    Update install

--- tomcat7/manifests/install.pp
+++ tomcat7/manifests/install.pp
@@ -1,38 +1,40 @@
 class tomcat7::install (
-  $tomcat_path = $tomcat7::params::tomcat_path,
-  $tomcat_package = $tomcat7::params::tomcat_package,
-  $tomcat_user = $tomcat7::params::tomcat_user,
-  $tomcat_pass = $tomcat7::params::tomcat_pass ) inherits tomcat7::params {
+  $tomcat_path        = $tomcat7::params::tomcat_path,
+  $tomcat_package     = $tomcat7::params::tomcat_package,
+  $tomcat_user        = $tomcat7::params::tomcat_user,
+  $tomcat_pass        = $tomcat7::params::tomcat_pass,
+  $tomcat_ensure      = $tomcat7::params::tomcat_ensure,
+  $tomcat_user_ensure = $tomcat7::params::tomcat_user_ensure) inherits tomcat7::params {
 
-file {
+  file {
     "${tomcat_path}/${tomcat_package}":
-      ensure => present,
-      source => "puppet:///modules/tomcat7/${tomcat_package}",
-      mode   => '0755',
+      ensure  => $tomcat_ensure,
+      source  => "puppet:///modules/tomcat7/${tomcat_package}",
+      mode    => '0755',
       require => Class["java"]
-  }
-  user {
-    'user_tomcat':
-        ensure   => present,
-        name     => $tomcat_user,
-        password => $tomcat_pass,
-        home    => '/home/tomcat',
- }
-
+  }->
+  
   exec {
     'Install_tomcat':
-        path    => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-        cwd     => $tomcat_path,
-        command => "tar -xzf ${tomcat_path}/${tomcat_package}",
-        require => [File["${tomcat_path}/${tomcat_package}"], User['user_tomcat']];
-
-     'tomcat_owner':
-        path    => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-        cwd     => $tomcat_path,
-        command => "/bin/chown -R ${tomcat_user}. ${tomcat_path}",
-        require => Exec['Install_tomcat']
-        
-    }
+      path    => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+      cwd     => $tomcat_path,
+      command => $tomcat_extract"tar -xzf ${tomcat_path}/${tomcat_package}",
+  }->
+  
+  exec {
+    'tomcat_owner':
+      path    => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+      cwd     => $tomcat_path,
+      command => "/bin/chown -R ${tomcat_user}. ${tomcat_path}",
+  }
+  
+  user {
+    'user_tomcat':
+      ensure   => $tomcat_user_ensure,
+      name     => $tomcat_user,
+      password => $tomcat_pass,
+      home     => '/home/tomcat',
+ }
 
 }
 
